bf50e68e2966a5458710797a781bb246910a77f4
Grattours <grattours@gmail.com>
2018-12-20 16:09:30 +0100

    Initial Commit

---
+++ RPG-P3-OC/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  RPG-P3-OC
+//
+//  Created by Luc Derosne on 20/12/2018.
+//  Copyright © 2018 Luc Derosne. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
