b73891a6d5d4f62bccbc09422bb78d5f85d1a6d3
Grattours <grattours@gmail.com>
2018-12-21 21:40:57 +0100

    Ajout fichiers

---
+++ RPG-P3-OC/Game.swift
@@ -0,0 +1,22 @@
+//
+//  game.swift
+//  RPG-P3-OC
+//
+//  Created by Luc Derosne on 20/12/2018.
+//  Copyright © 2018 Luc Derosne. All rights reserved.
+//
+
+import Foundation
+
+class Game{
+    // Equipes (class ?) paramétrer le nombre de personnage ?
+    // un jeu est un ensemble de batailles (à compter)
+    var team1 = Team()
+    var team2 = Team()
+    // Personnages
+    // presentation
+    // bataille
+    // suite
+    
+    
+}
---
+++ RPG-P3-OC/Team.swift
@@ -0,0 +1,20 @@
+//
+//  team.swift
+//  RPG-P3-OC
+//
+//  Created by Luc Derosne on 20/12/2018.
+//  Copyright © 2018 Luc Derosne. All rights reserved.
+//
+
+import Foundation
+
+class Team {
+    // l'Equipe et le joueur sont assimilables - même nom par exemple.
+    // 2 équipes (paramètrer le nombre ?)
+    // nb personnages init 3
+    // personnages - tableau
+    // statut (enjeu / hors jeu)
+    // suite
+}
+
+
