fe4c761d56ff59dbfe34b86ed2e804018a884352
Grattours <grattours@gmail.com>
2018-12-21 20:54:44 +0100

    Boucle principale d'action - Conflit - Battle - Fight  dans le main.swift

--- RPG-P3-OC/main.swift
+++ RPG-P3-OC/main.swift
@@ -2,20 +2,41 @@
 //  main.swift
 //  RPG-P3-OC
 //
-//  Created by Luc Derosne on 20/12/2018.
+//  Created by Luc Derosne on 21/12/2018.
 //  Copyright © 2018 Luc Derosne. All rights reserved.
 //
 
 import Foundation
 
+// conflict game battle fight
+var isWarOn: Bool = true
+var reply: String = ""
 print("Jeu de rôles - P3 - formation OC")
-print("================================")
-print(" Game started ")
-// instancier un game
-// créer équipes et joueurs
-print("  ... currect game ... ")
-// jouer
-// présenter vainqueur et scores
+print("==== S I M U L A T I O N =======")
+while isWarOn {
+    isWarOn = true
+    // boule partie
+    print(" Game started ")
+    // instancier un game
+    print(" création équipes et personnages")
+    print("  ... currect game ... ")
+    print(" ....jeu - batailles ")
+    print(" ... Equipe vainqueur - scores")
+    // déclencher une nouvelle guerre ?
+    print ("Voulez vous déclencher un nouveau conflit (O/N) ?")
+    repeat { reply = readLine() ?? "N"} while reply != "O" && reply != "N"
+    // boucler sur une autre partie ?
+    // fin boule partie
+    switch reply {
+    case "O" :
+        isWarOn = true
+    case "N" :
+        isWarOn = false
+    default :
+        print("choix incorrect")
+    }
 
-print("  Game Over ! ")
-// boucler sur une autre partie ?
+}
+
+print("au revoir")
+print("  La guerre est finie ! ")
